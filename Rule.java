62c0c543f1dfe6527dbdab0c228bbeffe54430b6
bhavanasingh <bhavanapbisht@gmail.com>
2013-04-15 20:31:10 -0500

    Update Rule.java

--- Rule.java
+++ Rule.java
@@ -1,6 +1,7 @@
+//package inventory;
 import java.util.*;
 import java.sql.*;
-
+import de.bezier.data.sql.*;
 
 /* class for adding rules for the medicine stock limits*/
 //If Existing rule for a item is there it updates the item rule amount else it adds a new rule.
@@ -8,38 +9,55 @@ import java.sql.*;
 
 
 public class Rule{
-  public Rule(int id,int amount)
+  
+  protected int Item_id = 0;
+  protected int qty = 0;
+        public ArrayList<Rule> getRules(MySQL msql){
+               ArrayList<Rule> data = new ArrayList<Rule>();
+               int i = 0;
+          try{
+                  
+                  Rule rl;
+                 msql.connect();
+                 String selectQuery = "Select * from Item order by Item_id";
+                 msql.query(selectQuery);
+                 
+                 while (msql.next()){
+                    rl = new Rule();
+                    rl.Item_id = Integer.parseInt(msql.getString(1));
+                    rl.qty = Integer.parseInt(msql.getString(2));
+                    data.add(rl);
+                    i += 1;
+                 }
+                 
+                 return data;
+             }
+             catch(Exception e)
+            {
+              System.out.println(e);
+            }
+            return data;
+        }
+        
+        Rule (){
+          
+        }
+        
+	Rule(int id,int amount, MySQL msql)
 	{
-		Connection con1;
-		
+		Connection con1 = null;
+                
 		try{
-		con1 = DriverManager.getConnection("jdbc:mysql://localhost/inventory", "root", "password");
-//		System.out.println("Connected to the database");
-		Statement stmt=con1.createStatement();
-		Statement stmtinsert=con1.createStatement();
-		Statement stmtupdate=con1.createStatement();
-				
-		String query="select count(*) from Rule where Item_id="+id+";";
-		ResultSet rs=stmt.executeQuery(query);
-		
-		while(rs.next())
-		{
-		if(rs.getInt(1)==0)
-		{
-			String queryinsert="insert into Rule values("+id+","+amount+");";
-			stmtinsert.executeUpdate(queryinsert);
+        		//System.out.println("Connected to the database");
+                        //con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory?user=root&password=bs140209");
+                        msql.connect();
+        		System.out.println("Connected to the database");
+        		msql.execute("insert into Rule values("+id+","+amount+");");
+
 		}
-		else
-		{
-			String queryu="update Rule set Consumption_per_month="+amount+" where Item_id="+id+";";
-			stmtupdate.executeUpdate(queryu);
-		}
-			
-		}
-		}
-	catch(Exception e)
-	{
-		System.out.println("entered");
-	}
+        	catch(Exception e)
+        	{
+        		System.out.println(e);
+        	}
 	}
 }
